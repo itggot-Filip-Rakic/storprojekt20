a5b52b67595d77cb84f88b2e053cfbb96e46ed70
Filip Rakic <filip.rakic@learnet.se>
2020-03-19 15:40:23 +0100

    login & before block

--- app.rb
+++ app.rb
@@ -7,6 +7,23 @@ require 'net/http'
 require_relative './model.rb'
 enable :sessions
 
+
+before do 
+    if session[:user_id] != nil 
+        redirect('/')
+    end
+        if request.post?
+            if session[:last_action].nil?
+                session[:last_action] = Time.now
+            end
+            if ((session[:last_action] + 2) > Time.now())
+                sleep(1)
+            end
+            session[:last_action] = Time.now()
+            session[:regerror] = nil
+        end
+    end
+
 get("/") do 
     slim(:index)
 end
@@ -43,19 +60,25 @@ end
 post("/users/login") do
     username = params[:username]
     password = params[:password]
-    session[:logged_in] = nil
-   
-    response = login_verify(username, password)
-
-    if response.successful
-        session[:logged_in] = username
-        redirect("/")
-    else
-        redirect("/error")
+    
+    existing_username = get_from_db("username","user","username",username)
+    
+    if existing_username.empty?
+        session[:login_error] = "Username or password wrong"
+        redirect("/users/error")
     end
+
+    password_for_user = get_from_db("password_digest","user","username",username)[0]["password_digest"]
+
+    if BCrypt::Password.new(password_for_user) != password
+        session[:login_error] = "Username or password wrong"
+        redirect("/users/error")
+    end
+
+    session[:user_id] = get_from_db("user_id","user","username",username)[0]["user_id"]
 end
 
 post("/users/logout") do 
-    session[:logged_in] = nil
+    session.destroy
     redirect("/")
 end
